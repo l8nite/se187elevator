3620ab9224d433e63ea77767a52813eb0494a9a8
Shaun Guth <shaun@smial.(none)>
2011-09-24 19:41:10 -0700

    Lotssss more cleanup

--- SourceCode/DoorPanel/src/DoorCommand.java
+++
@@ -1,4 +0,0 @@
-
-public enum DoorCommand {
-	OPEN, CLOSE;
-}
---
+++ se187elevator/FloorPanel_v1/src/FloorPanelFactory.java
@@ -0,0 +1,28 @@
+
+public class FloorPanelFactory 
+{
+	public static IFloorPanel createFloorPanel()
+	{
+		IFloorPanel ifloorPanel = null;
+		if(FloorPanelConfiguration.getfloorPanelQueueType().equalsIgnoreCase("default"))
+		{
+			ifloorPanel = new FloorPanel();
+			ifloorPanel.setFloorPanelQueueType(new FloorPanelQueue());
+			ifloorPanel.setButtonColor(FloorPanelConfiguration.getButtonColor());
+			ifloorPanel.setActiveButtonColor(FloorPanelConfiguration.getActiveButtonColor());;
+		}
+		
+
+		if (FloorPanelConfiguration.getTextType().equalsIgnoreCase("text")){
+			ifloorPanel.setUpText("UP");
+			ifloorPanel.seDownText("DOWN");
+		}else if (FloorPanelConfiguration.getTextType().equalsIgnoreCase("symbol")){
+			ifloorPanel.setUpText("^");
+			ifloorPanel.seDownText("v");
+		
+		}
+		
+			
+		return ifloorPanel;
+	}
+}
